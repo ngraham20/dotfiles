d9c17411c1f3626e42a0ec2cb8391a44f6b95678
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2023-07-24 10:35:46 -0600

    changed focused color

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -89,7 +89,7 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 -- Border colors for unfocused and focused windows, respectively.
 --
 myNormalBorderColor  = "#dddddd"
-myFocusedBorderColor = "#ff0000"
+myFocusedBorderColor = "#1DDFF2"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
